4df931278d6f461b3429ab78d5a7895aa3b2f184
xzuyn <16216325+xzuyn@users.noreply.github.com>
2023-07-27 13:43:36 -0400

    Update run_llama-lora-tuner.sh

--- LLaMa-LoRA-Tuner/run_llama-lora-tuner.sh
+++ LLaMa-LoRA-Tuner/run_llama-lora-tuner.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-read -p "HF ID or Model Directory: " base_model_input
+read -p "HF ID or Model Directory (Required): " base_model_input
 
 if [ -z "$base_model_input" ]; then
   echo "Base model input cannot be empty."
   exit 1
+fi
+
+read -p "WandB API Key (Optional): " wandb_api_key_input
+
+if [ -z "$wandb_api_key_input" ]; then
+  wandb_arg=" "
+else
+  wandb_arg=" --wandb_api_key=$wandb_api_key_input "
 fi
 
 source rocm_venv/bin/activate
@@ -12,4 +20,4 @@ export HIP_VISIBLE_DEVICES=0
 export HSA_OVERRIDE_GFX_VERSION=11.0.0
 export TOKENIZERS_PARALLELISM=false
 
-python app.py --data_dir='./data' --base_model="$base_model_input" --skip_loading_base_model=True
+python app.py --data_dir='./data' --base_model="$base_model_input"$wandb_arg--skip_loading_base_model=True
