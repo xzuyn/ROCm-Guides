9a03f90c8db55edb0f7fe1c7317c0e49a5b66312
xzuyn <16216325+xzuyn@users.noreply.github.com>
2023-07-27 13:33:50 -0400

    Update run_llama-lora-tuner.sh

--- LLaMa-LoRA-Tuner/run_llama-lora-tuner.sh
+++ LLaMa-LoRA-Tuner/run_llama-lora-tuner.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo "HF ID or Model Directory: "
-read base_model_input
+read -p "HF ID or Model Directory: " base_model_input
 
 if [ -z "$base_model_input" ]; then
   echo "Base model input cannot be empty."
@@ -13,5 +12,4 @@ export HIP_VISIBLE_DEVICES=0
 export HSA_OVERRIDE_GFX_VERSION=11.0.0
 export TOKENIZERS_PARALLELISM=false
 
-python app.py --data_dir='./data' --base_model="$base_model_input"
-
+python app.py --data_dir='./data' --base_model="$base_model_input" --skip_loading_base_model=True
