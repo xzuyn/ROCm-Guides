e4d25bf3859cf8102bbe87729257e32270a5a48a
xzuyn <16216325+xzuyn@users.noreply.github.com>
2023-08-14 19:09:36 -0400

    Update setup_rocm_venv_whl.sh

--- LLaMa-LoRA-Tuner/setup_rocm_venv_whl.sh
+++ LLaMa-LoRA-Tuner/setup_rocm_venv_whl.sh
@@ -11,4 +11,14 @@ pip uninstall -y torch
 
 pip install --pre torch --index-url https://download.pytorch.org/whl/nightly/rocm5.6
 
+pip uninstall bitsandbytes
+cd rocm_venv
+git clone https://github.com/arlo-phoenix/bitsandbytes-rocm-5.6.git bitsandbytes
+cd bitsandbytes
+export ROCM_HOME=/opt/rocm-5.6.0
+make hip ROCM_TARGET=gfx1100
+pip install .
+cd ..
+cd ..
+
 echo "Setup complete."
