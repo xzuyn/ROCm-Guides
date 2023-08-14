f58e31cb78cdf53930796b4d1569124c6b53deba
xzuyn <16216325+xzuyn@users.noreply.github.com>
2023-08-14 19:06:55 -0400

    Update setup_rocm_venv_build.sh

--- kohya_ss/setup_rocm_venv_build.sh
+++ kohya_ss/setup_rocm_venv_build.sh
@@ -1,80 +1,101 @@
 #!/bin/bash
 
-# Step 1/11: Creating and activating a Python virtual environment
+# Step 1/14: Creating and activating a Python virtual environment
 echo ""
-echo "Step 1/11: Creating and activating a Python virtual environment"
+echo "Step 1/14: Creating and activating a Python virtual environment"
 echo ""
 python3 -m venv venv
 source venv/bin/activate
 
-# Step 2/11: Installing requirements
+# Step 2/14: Installing requirements
 echo ""
-echo "Step 2/11: Installing requirements"
+echo "Step 2/14: Installing requirements"
 echo ""
 pip install --use-pep517 -r requirements.txt
 
-# Step 3/11: Removing unwanted PyTorch and TorchVision installations
+# Step 3/14: Removing unwanted PyTorch and TorchVision installations
 echo ""
-echo "Step 3/11: Removing unwanted PyTorch and TorchVision installations"
+echo "Step 3/14: Removing unwanted PyTorch and TorchVision installations"
 echo ""
 pip uninstall -y torch torchvision
 
-# Step 4/11: Cloning the PyTorch repo
+# Step 4/14: Cloning the PyTorch repo
 echo ""
-echo "Step 4/11: Cloning PyTorch"
+echo "Step 4/14: Cloning PyTorch"
 echo ""
 cd venv
 git clone https://github.com/pytorch/pytorch
 
-# Step 5/11: Updating submodules
+# Step 5/14: Updating submodules
 echo ""
-echo "Step 5/11: Updating submodules"
+echo "Step 5/14: Updating submodules"
 echo ""
 cd pytorch
 git submodule sync
 git submodule update --init --recursive
 
-# Step 6/11: Installing requirements
+# Step 6/14: Installing requirements
 echo ""
-echo "Step 6/11: Installing requirements"
+echo "Step 6/14: Installing requirements"
 echo ""
 python -m pip install --upgrade pip wheel
 pip install -r requirements.txt
 
-# Step 7/11: Building the AMD-specific parts of PyTorch
+# Step 7/14: Building the AMD-specific parts of PyTorch
 echo ""
-echo "Step 7/11: Building the AMD-specific parts of PyTorch"
+echo "Step 7/14: Building the AMD-specific parts of PyTorch"
 echo ""
 python tools/amd_build/build_amd.py
 
-# Step 8/11: Compiling PyTorch
+# Step 8/14: Compiling PyTorch
 echo ""
-echo "Step 8/11: Building the AMD-specific parts of PyTorch"
+echo "Step 8/14: Building the AMD-specific parts of PyTorch"
 echo ""
 cmake_p_p=$(python -c "import site; print(site.getsitepackages()[0])")
 export CMAKE_PREFIX_PATH="$cmake_p_p"
 BUILD_TEST=0 USE_CUDA=0 USE_CUDNN=0 USE_ROCM=1 PYTORCH_ROCM_ARCH=gfx1100 USE_MKLDNN=0 python setup.py develop
 export HIP_VISIBLE_DEVICES=0
 
-# Step 9/11: Cloning the TorchVision repo
+# Step 9/14: Cloning the TorchVision repo
 echo ""
-echo "Step 9/11: Cloning TorchVision"
+echo "Step 9/14: Cloning TorchVision"
 echo ""
 cd ..
 git clone https://github.com/pytorch/vision
 
-# Step 10/11: Compiling TorchVision
+# Step 10/14: Compiling TorchVision
 echo ""
-echo "Step 9/11: Compiling TorchVision"
+echo "Step 10/14: Compiling TorchVision"
 echo ""
 cd vision
 python setup.py install
+
+# Step 11/14: Removing unwanted bitsandbytes installation
+echo ""
+echo "Step 11/14: Removing unwanted bitsandbytes installation"
+echo ""
 cd ..
+pip uninstall bitsandbytes
+
+# Step 12/14: Cloning the bitsandbytes-rocm fork repo
+echo ""
+echo "Step 12/14: Cloning the bitsandbytes-rocm fork repo"
+echo ""
+git clone https://github.com/arlo-phoenix/bitsandbytes-rocm-5.6.git bitsandbytes
+cd bitsandbytes
+
+# Step 13/14: Installing bitsandbytes-rocm
+echo ""
+echo "Step 13/14: Installing bitsandbytes-rocm"
+echo ""
+export ROCM_HOME=/opt/rocm-5.6.0
+make hip ROCM_TARGET=gfx1100
+pip install .
 cd ..
 
-# Step 11: Configuring environment variables
+# Step 14/14: Configuring environment variables
 echo ""
-echo "Step 11/11: Configuring environment variables"
+echo "Step 14/14: Configuring environment variables"
 echo ""
 
 # Check if .bashrc exists, if not, create one
