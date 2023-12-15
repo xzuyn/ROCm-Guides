176de135249f263ac29a54fc1b2cc48ffac5d838
Kademo15 <95079115+Kademo15@users.noreply.github.com>
2023-12-15 17:09:18 +0100

    Create setup_rocm_venv_whl.sh

---
+++ ComfyUi/setup_rocm_venv_whl.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Step 1/6: Updating submodules
+echo ""
+echo "Step 1/6: Updating submodules."
+echo ""
+git submodule sync
+git submodule update --init --recursive
+
+# Step 2/6: Creating and activating a Python virtual environment
+echo ""
+echo "Step 2/6: Creating and activating a Python virtual environment"
+echo ""
+python3 -m venv venv
+source venv/bin/activate
+
+# Step 3/6: Installing the PyTorch and TorchVision we actually want
+echo ""
+echo "Step 3/6: Installing PyTorch and TorchVision"
+echo ""
+pip install --pre torch torchvision --index-url https://download.pytorch.org/whl/nightly/rocm5.7
+
+# Step 4/6: Installing requirements.txt (will ignore the install torch version hopefully)
+echo ""
+echo "Step 4/6: Installing requirements.txt"
+echo ""
+pip install -r requirements.txt
+
+# Step 6/6: Configuring environment variables
+echo ""
+echo "Step 6/6: Configuring environment variables"
+echo ""
+
+# Check if .bashrc exists, if not, create one
+if [ ! -e ~/.bashrc ]; then
+    touch ~/.bashrc
+fi
+
+# Check if HSA_OVERRIDE_GFX_VERSION is already set
+if grep -q "HSA_OVERRIDE_GFX_VERSION=11.0.0" ~/.bashrc; then
+    echo "HSA_OVERRIDE_GFX_VERSION is already set in .bashrc"
+else
+    echo "Adding HSA_OVERRIDE_GFX_VERSION to .bashrc..."
+    echo "export HSA_OVERRIDE_GFX_VERSION=11.0.0" >> ~/.bashrc
+    echo "HSA_OVERRIDE_GFX_VERSION added to .bashrc"
+fi
+
+echo 'You can now run ComfyUi to see how to launch it look into the README at step 5'
