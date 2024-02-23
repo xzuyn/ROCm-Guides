3c44fca7d3e372fe62ee9eb36aebff2e1533153a
Kademo15 <95079115+Kademo15@users.noreply.github.com>
2024-02-23 18:17:33 +0100

    Update setup_rocm_venv_whl.sh
    Settting the enviroment variables first is better because otherwise the user has to ctrl-c out of the running automatic for the script to continue.

--- VLAD_SD.Next/setup_rocm_venv_whl.sh
+++ VLAD_SD.Next/setup_rocm_venv_whl.sh
@@ -1,41 +1,6 @@
 #!/bin/bash
 
-
-# Step 1/6: Cloning the repository
-echo ""
-echo "Step 1/6: Cloning the repository"
-echo ""
-git clone https://github.com/vladmandic/automatic
-cd automatic
-
-# Step 2/6: Updating submodules
-echo ""
-echo "Step 2/6: Updating submodules"
-echo ""
-git submodule sync
-git submodule update --init --recursive
-
-# Step 3/6: Creating and activating a Python virtual environment
-echo ""
-echo "Step 3/6: Creating and activating a Python virtual environment"
-echo ""
-python3 -m venv venv
-source venv/bin/activate
-
-#Step 4/6: Installing the right PyTorch and TorchVision versions and other requirements
-echo ""
-echo "Step 4/6: Installing the right PyTorch and TorchVision versions and other requirements"
-echo ""
-pip install -r requirements.txt
-pip install --pre torch torchvision --index-url https://download.pytorch.org/whl/nightly/rocm6.0
-
-# Step 5/6: Launching the Python script
-echo ""
-echo "Step 5/6: Launching the Python script"
-echo ""
-python launch.py
-
-# Step 6/6: Configuring environment variables
+# Step 1/6: Configuring environment variables
 echo ""
 echo "Step 6/6: Configuring environment variables"
 echo ""
@@ -54,4 +19,38 @@ else
     echo "HSA_OVERRIDE_GFX_VERSION added to .bashrc"
 fi
 
+# Step 2/6: Cloning the repository
+echo ""
+echo "Step 1/6: Cloning the repository"
+echo ""
+git clone https://github.com/vladmandic/automatic
+cd automatic
+
+# Step 3/6: Updating submodules
+echo ""
+echo "Step 2/6: Updating submodules"
+echo ""
+git submodule sync
+git submodule update --init --recursive
+
+# Step 4/6: Creating and activating a Python virtual environment
+echo ""
+echo "Step 3/6: Creating and activating a Python virtual environment"
+echo ""
+python3 -m venv venv
+source venv/bin/activate
+
+#Step 5/6: Installing the right PyTorch and TorchVision versions and other requirements
+echo ""
+echo "Step 4/6: Installing the right PyTorch and TorchVision versions and other requirements"
+echo ""
+pip install -r requirements.txt
+pip install --pre torch torchvision --index-url https://download.pytorch.org/whl/nightly/rocm6.0
+
+# Step 6/6: Launching the Python script
+echo ""
+echo "Step 5/6: Launching the Python script"
 echo 'You can now run SD.Next any time by running the "./webui.sh" bash script'
+echo ""
+python launch.py
+
